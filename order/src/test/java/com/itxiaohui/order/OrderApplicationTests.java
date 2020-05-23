792741cb8d10d90ac32fe2d29ac6957cade4d14b
zhoubanglin <1724599414@qq.com>
2020-05-23 23:27:16 +0800

    项目结构初始化

---
+++ order/src/test/java/com/itxiaohui/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itxiaohui.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
